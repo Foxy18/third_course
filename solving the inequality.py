b39581eb9d5aebffd3e2ac2415019b0645239c4b
Foxy18 <91536077+Foxy18@users.noreply.github.com>
2021-10-10 00:48:40 +0300

    Add files via upload

--- solving the inequality.py
+++ solving the inequality.py
@@ -2,9 +2,11 @@
 a, b = map(float, input('Введите коэффициенты a и b для неравенства ax ≥ b через пробел: ').split())
 if a > 0:
     print(f'x ∈ [{b / a}; +∞)')
-elif a < 0:
-    print(f'x ∈ (-∞; {b / a}]')
+elif a < 0 and b == 0:
+    print('x ∈ (-∞; 0]')
+elif a < 0 and b != 0:
+    print(f'x ∈ (-∞; {b / a})')
 elif a == 0 and b > 0:
-    print(f'x ∈ ø')
+    print('x ∈ ø')
 else:
     print('x ∈ (-∞; +∞)')
