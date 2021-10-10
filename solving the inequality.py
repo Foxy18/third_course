18839f8af255deb0d25acebd586a27efa49c6926
Foxy18 <91536077+Foxy18@users.noreply.github.com>
2021-10-10 03:28:26 +0300

    Add files via upload

--- solving the inequality.py
+++ solving the inequality.py
@@ -1,12 +1,10 @@
 #Составить программу решения неравенства ax ≥b, где a, b –параметры, значения которых задаются с клавиатуры.
 a, b = map(float, input('Введите коэффициенты a и b для неравенства ax ≥ b через пробел: ').split())
 if a > 0:
-    print(f'x ∈ [{b / a}; +∞)')
-elif a < 0 and b == 0:
-    print('x ∈ (-∞; 0]')
-elif a < 0 and b != 0:
-    print(f'x ∈ (-∞; {b / a})')
+    print(f"x ∈ [{b / a}; +∞)")
+elif a < 0:
+    print(f'x ∈ (-∞; {b / a}]' if b != 0 else 'x ∈ (-∞; 0]')
 elif a == 0 and b > 0:
-    print('x ∈ ø')
+    print("x ∈ ø")
 else:
-    print('x ∈ (-∞; +∞)')
+    print("x ∈ (-∞; +∞)")
