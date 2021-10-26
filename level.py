108be457e53cd0d2a9c55117928fe128d551893c
Foxy18 <91536077+Foxy18@users.noreply.github.com>
2021-10-26 10:20:29 +0300

    Add files via upload

--- level.py
+++ level.py
@@ -3,7 +3,7 @@ def product(k):
     return ((-1)**k) * ((2**k) / factorial(k)) 
 e, k = int(input()), 1
 summ = 0
-while product(k - 1) - product(k + 1) > e:
+while product(k) > e:
     summ += product(k)
     k += 1
 print(summ)
